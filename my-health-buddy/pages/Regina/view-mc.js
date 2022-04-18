9d6adb8ecdb6ef37778e289013be3ca1ee7597ca
Regina Yun <regina.yun.mei.lin@gmail.com>
2022-04-18 23:34:58 +0800

    updated clinic names

--- my-health-buddy/pages/Regina/view-mc.js
+++ my-health-buddy/pages/Regina/view-mc.js
@@ -9,7 +9,7 @@ const DATA = [
   {
     id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
     mc_id: '1323',
-    clinic: "First Clinic",
+    clinic: "Long & Yun Medical Clinic",
     mc_duration: '2 days',
     mc_startDate: '3rd Dec 2021',
     mc_endDate: '4th Dec 2021'
@@ -17,7 +17,7 @@ const DATA = [
   {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     mc_id: '943',
-    clinic: "Second Clinic",
+    clinic: "Long & Yun Medical Clinic",
     mc_duration: '1 day',
     mc_startDate: '30th Mar 2021',
     mc_endDate: '30th Mar 2021'
@@ -25,7 +25,7 @@ const DATA = [
   {
     id: "58694a0f-3da1-471f-bd96-145571e29d72",
     mc_id: '820',
-    clinic: "Third Clinic",
+    clinic: "Khoo & Tan Medical Clinic",
     mc_duration: '3 days',
     mc_startDate: '26th May 2020',
     mc_endDate: '28th May 2020'
@@ -33,7 +33,7 @@ const DATA = [
   {
     id: "3358761d-c7d9-4b5f-8372-a218b7ffede8",
     mc_id: '760',
-    clinic: "Third Clinic",
+    clinic: "Khoo & Tan Medical Clinic",
     mc_duration: '3 days',
     mc_startDate: '26th Sep 2019',
     mc_endDate: '28th Sep 2019'
@@ -41,7 +41,7 @@ const DATA = [
   {
     id: "5dfb5157-869e-4259-ba08-3a34cb4faf95",
     mc_id: '699',
-    clinic: "Second Clinic",
+    clinic: "Oboey Medical Clinic",
     mc_duration: '2 days',
     mc_startDate: '14th Jun 2019',
     mc_endDate: '14th Jun 2019'
@@ -49,7 +49,7 @@ const DATA = [
   {
     id: "35c53d0b-fd2d-45d4-98be-94c31c0d85f1",
     mc_id: '604',
-    clinic: "First Clinic",
+    clinic: "S&S Medical Clinic",
     mc_duration: '1 day',
     mc_startDate: '12th Jan 2019',
     mc_endDate: '13th Jan 2019'
@@ -57,7 +57,7 @@ const DATA = [
   {
     id: "8716c05a-0698-4ac9-821c-d54ac7354a2d",
     mc_id: '542',
-    clinic: "First Clinic",
+    clinic: "S&S Medical Clinic",
     mc_duration: '5 days',
     mc_startDate: '12th Jul 2018',
     mc_endDate: '16th Jul 2018'
