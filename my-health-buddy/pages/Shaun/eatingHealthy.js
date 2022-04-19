9f141c587500445faaef18cf23756c72076a0fd5
shaunboey <shaunboey@gmail.com>
2022-04-20 00:41:31 +0800

    having imported eatingHealthy page over, but will need to test something out in the next meeting

---
+++ my-health-buddy/pages/Shaun/eatingHealthy.js
@@ -0,0 +1,21 @@
+export default eatingHealthy = [
+  {
+    title: "1) Add fruits and fibre",
+    body: "What a lot of people don't realise is how fibre is neglected in our daily meals. Make sure to take at least 3 servings of fruits and vegetables daily",
+    imgUrl: "https://cdn.pixabay.com/photo/2022/03/20/08/41/vegetables-7080261_960_720.jpg"
+  },
+  {
+    title: "2) Don't forget your proteins",
+    body: "For a good calculation, just measure your body weight in pounds,and that's how much protein in grams you need daily",
+    imgUrl: "https://cdn.pixabay.com/photo/2020/10/02/17/58/protein-5621773_960_720.png"
+  },
+  {
+    title: "3) Carbohydrates are NOT bad for you",
+    body: "Carbohydrates like rice, potatoes, bread are important for giving your body energy. Contrary to popular belief, carbohydrates are essential in a balanced diet, but portion control is recommended",
+    imgUrl: "https://cdn.pixabay.com/photo/2020/12/01/18/12/bread-5795166_960_720.jpg"
+  },
+  {title: "4) See a doctor if unsure",
+    body: "If you are unsure about your health or what to do, it's always good to consult a doctor or a licensed nutritionist. If in doubt it's always better to seek professional help",
+    imgUrl: "https://cdn.pixabay.com/photo/2020/05/25/03/37/doctor-5216835_960_720.png"
+}
+]
