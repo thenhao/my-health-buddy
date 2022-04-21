a5c8195f034d789dad4099dc42c3ce519a524f81
Jeffreytanhk <94664181+Jeffreytanhk@users.noreply.github.com>
2022-04-21 10:22:38 +0800

    update of article page 3

--- my-health-buddy/pages/Shaun/exercisingWell.js
+++ my-health-buddy/pages/Shaun/exercisingWell.js
@@ -1,6 +1,6 @@
 export default exercisingWell = [
   {
-    title: "1) Add fruits and fibre",
+    title: "1) Work Out Routine",
     body: "What a lot of people don't realise is how fibre is neglected in our daily meals. Make sure to take at least 3 servings of fruits and vegetables daily",
     imgUrl: "https://cdn.pixabay.com/photo/2022/03/20/08/41/vegetables-7080261_960_720.jpg"
   },
@@ -14,8 +14,9 @@ export default exercisingWell = [
     body: "Carbohydrates like rice, potatoes, bread are important for giving your body energy. Contrary to popular belief, carbohydrates are essential in a balanced diet, but portion control is recommended",
     imgUrl: "https://cdn.pixabay.com/photo/2020/12/01/18/12/bread-5795166_960_720.jpg"
   },
-  {title: "4) See a doctor if unsure",
+  {
+    title: "4) See a doctor if unsure",
     body: "If you are unsure about your health or what to do, it's always good to consult a doctor or a licensed nutritionist. If in doubt it's always better to seek professional help",
     imgUrl: "https://cdn.pixabay.com/photo/2020/05/25/03/37/doctor-5216835_960_720.png"
-}
+  }
 ]
